7f6b2dcf0083917ca29efbfb69e7f9996ebe58cb
OwenFeik <owenhfeik@gmail.com>
2018-10-31 17:49:15 +1100

    random card, edition searching

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
-from scryfall import get_uri
+from random import shuffle
+from scryfall import get_uri,get_random_uri,get_similar,get_random_from_set,get_printing
 
 #Get the client token
 with open('token.txt', 'r') as f:
@@ -7,6 +8,16 @@ with open('token.txt', 'r') as f:
 
 #Start the client.
 client = discord.Client()
+
+#Capitalise a card name
+def capitalise(name):
+    out=''
+    for i in range(0,len(name)):
+        if (name[i].isalnum() and not name[i-1].isalnum()) or i==0:
+            out+=name[i].capitalize()
+        else:
+            out+=name[i]
+    return out
 
 @client.event
 async def on_message(message):
@@ -16,35 +27,108 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    #If the message contains a card tag
+    #Handles card tags
     if '[' in message.content and ']' in message.content:
         
         #Iterate through the message to find all card names
         card_names=[]
         is_query=False
+        is_set=False
         card_name=''
+        set_name=''
         for c in message.content:
             if c=='[':
                 is_query=True
             elif c==']':
                 is_query=False
+                is_set=False
+                if card_name:
+                    card_names.append(card_name)
+                    card_name=''
+                elif set_name:
+                    card_names[len(card_names)-1]=[card_names[len(card_names)-1],set_name]
+                    set_name=''
+            elif c=='|':
+                is_query=False
+                is_set=True
                 card_names.append(card_name)
                 card_name=''
+            elif is_set:
+                set_name+=c
             elif is_query:
                 card_name+=c
 
-        #Find and send one or more messages for each card.
-        for i in range(0,len(card_names)):
-            uris=get_uri(card_names[i])
-            if uris:
-                for uri in uris:
-                    img=discord.Embed()
-                    img.set_image(url=uri)
-                    print('Found '+uri)
-                    await client.send_message(message.channel,embed=img)
+                #[nightmare|m15]
+
+        #Find images and send one or more messages for each card.
+        for card in card_names:
+            #If it's a list, the request specified an edition
+            if type(card)==list:
+                name=card[0].lower()
+                ed=card[1].upper()
             else:
-                print('Failed to find '+card_names[i])
-                await client.send_message(message.channel,content=card_names[i]+' not found :cry:')
+                name=card.lower()
+                ed=''
+            
+            #If an edition was specified
+            if ed:
+                if name=='random': #If they want random, get random card.
+                    uri=get_random_from_set(ed)
+                    if uri:
+                        img=discord.Embed().set_image(url=uri)
+                        await client.send_message(message.channel,embed=img)
+                    else: #If we don't get a uri, the search failed and the set doesn't exist.
+                        await client.send_message(message.channel,content=f"Couldn't find set {ed} :cry:")
+                else:
+                    found,data=get_printing(name,ed)
+                    if found=='card':
+                        for uri in data:
+                            img=discord.Embed().set_image(url=uri)
+                            print(f'Found {uri}')
+                            await client.send_message(message.channel,embed=img)
+                    elif found=='suggs':
+                        print(f'Failed to find {capitalise(name)} in {ed}')
+                        if len(data)>5:
+                            data=shuffle(data)[0:5].sort()
+                        msg=f"Couldn't find {capitalise(name)}. Maybe you meant:\n" 
+                        for name in data:
+                            msg+=name+'\n'
+                        print('Found alternatives.')
+                        await client.send_message(message.channel,content=msg[0:len(msg-1)])
+                    else:
+                        print(f'Failed to find {capitalise(name)} in {ed}')
+                        await client.send_message(message.channel,content=f'{capitalise(name)} not found in {ed} :cry:')
+
+            else:
+                if name=='random':
+                    img=discord.Embed()
+                    img.set_image(url=get_random_uri())
+                    await client.send_message(message.channel,embed=img)
+                elif name=='best card' or name=='the best card':
+                    img=discord.Embed().set_image(url=get_uri('Kalonian Hydra'))
+                    print('Kalonian Hydra is the best card.')
+                    await client.send_message(message.channel,embed=img)
+                else:
+                    uris=get_uri(name)
+                    if uris:
+                        for uri in uris:
+                            img=discord.Embed().set_image(url=uri)
+                            print(f'Found {uri}')
+                            await client.send_message(message.channel,embed=img)
+                    else:
+                        print(f'Failed to find {capitalise(name)}')
+                        suggs=get_similar(name)
+                        if suggs:
+                            if len(suggs)>5:
+                                shuffle(suggs)
+                                suggs=suggs[0:5]
+                            msg=f"Couldn't find {capitalise(name)}. Maybe you meant:\n\n" 
+                            for name in suggs:
+                                msg+='\t'+name+'\n'
+                            print('Found alternatives.')
+                            await client.send_message(message.channel,content=msg[0:len(msg)-1])
+                        else:
+                            await client.send_message(message.channel,content=capitalise(name)+' not found :cry:')
 
     #Handles commands (--)
     if message.content.startswith('--'):
@@ -54,6 +138,8 @@ async def on_message(message):
             msg='Use [Card Name] to call me!'
         elif message.content.startswith('--easteregg'):
             msg='Smartarse'
+        elif message.content.startswith('--all'):
+            msg='All commands:\n--all\n--hello\n--help'
         else:
             msg='OwO, what\'s this? I don\'t understand that! Maybe try --help'
         await client.send_message(message.channel,msg)
--- scryfall.py
+++ scryfall.py
@@ -1,5 +1,6 @@
 import requests
 
+#Get uri of a cardname query
 def get_uri(query):
     request='https://api.scryfall.com/cards/named?fuzzy='+query.replace(' ', '+') #Use fuzzy for partial matching
     r=requests.get(request).json() #Get the response as a dictionary
@@ -8,3 +9,40 @@ def get_uri(query):
     elif r['layout']=='transform' or r['layout']=='double_faced_token': #If DFC get both faces
         return [r['card_faces'][i]['image_uris']['normal'] for i in range(0,2)]
     return [r["image_uris"]["normal"]] #If normal card, get image
+
+
+#Get uri of a specific printing of card
+def get_printing(query,ed):
+    request=f'https://api.scryfall.com/cards/search?q=e%3D{ed}+'+query.replace(' ','+') #Search for cards in specificied edition
+    r=requests.get(request).json() #Get data as dictionary
+    if 'status' in r and r['status']==404: #If card doesn't exist
+        return 'fail',False #We return a few data types to save on api calls
+    elif len(r['data'])>1: #If there are more than 1 matches
+        return 'suggs',[r['data'][i]['name'] for i in range(0,len(r['data']))] #List of card names to print as suggestions
+    else:
+        if r['data'][0]['layout']=='transform' or r['data'][0]['layout']=='double_faced_token': #If its a DFC get both sides
+            return 'card',[r['data'][i]['image_uris']['normal'] for i in range(0,2)]
+        return 'card',[r['data'][0]['image_uris']['normal']] #Normal card, give card image
+
+#Get a random card uri
+def get_random_uri():
+    request='https://api.scryfall.com/cards/random'
+    r=requests.get(request).json()
+    return r['image_uris']['normal']
+
+#Get a random card froma set
+def get_random_from_set(ed):
+    request='https://api.scryfall.com/cards/random?q=e%3D'+ed
+    r=requests.get(request).json()
+    if 'status' in r and r['status']==404:
+        return False
+    else:
+        return r['image_uris']['normal']
+
+#Get suggestions similar to a card
+def get_similar(query):
+    request='https://api.scryfall.com/cards/search?q='+query.replace(' ','+') #Search for the card
+    r=requests.get(request).json() #Get data as json
+    if 'status' in r and r['status']==404:  #If card not found return false
+        return False
+    return [r['data'][i]['name'] for i in range(0,len(r['data']))] #Otherwise, return a list of similar names
