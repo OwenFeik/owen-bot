a6b2e34a7c48d4e76d468914774797e225dcfb11
OwenFeik <owenhfeik@gmail.com>
2020-06-08 00:07:18 +1000

    fix kick, flatten a little

--- kick.py
+++ kick.py
@@ -1,6 +1,4 @@
 import time
-import asyncio
-
 import discord
 
 class CommandHandler():
@@ -10,47 +8,46 @@ class CommandHandler():
 
     async def handle_command(self, message):
         self.scrub_votes()
-        if message.mentions:
-            if len(message.mentions) > 1:
-                return 'I can only kick one person at a time!'
-            target = message.mentions[0]
-
-            if type(target) != discord.Member:
-                return 'Sorry, I can\'t find voice information for ' + \
-                    f'{target.display_name}.'
-
-            if target.voice is None:
-                return f'{target.display_name} isn\'t in a voice channel.'
-
-            channel = target.voice.channel
-            if channel is None or channel != message.author.voice.channel:
-                return 'You can\'t kick people you aren\'t' + \
-                    'in a voice channel with!'
-
-            voice_members = len(channel.members)
-            if voice_members < 3:
-                return 'Sorry, I don\'t kick people from voice channels ' + \
-                    'with less than 3 members.'
-            required_votes = int(voice_members / 2 + 1)
-
-            self.add_vote(target, message.author, channel)
-            vote_count = self.vote_count(target, channel)
-
-            if vote_count >= required_votes:
-                try:
-                    await target.move_to(None, 
-                        reason='Democracy is a beautiful thing.')
-                except discord.Forbidden:
-                    return 'Tragically, I don\'t have permission to do that.'
-                
-                self.scrub_votes(target)
-                return 'The council has spoken. ' + \
-                    f'{target.mention} has been disconnected.'
-            else:
-                needed_votes = required_votes - vote_count
-                return f'Vote received! {needed_votes} more votes required.'
-        else:
+        if not message.mentions:
             return 'Usage: "--kick <mention>" e.g. "--kick @BadPerson".'
+        if len(message.mentions) > 1:
+            return 'I can only kick one person at a time!'
+        target = message.mentions[0]
+
+        if type(target) != discord.Member:
+            return 'Sorry, I can\'t find voice information for ' + \
+                f'{target.display_name}.'
+
+        if target.voice is None:
+            return f'{target.display_name} isn\'t in a voice channel.'
+
+        channel = target.voice.channel
+        if channel is None or channel != message.author.voice.channel:
+            return 'You can\'t kick people you aren\'t' + \
+                'in a voice channel with!'
+
+        voice_members = len(channel.members)
+        if voice_members < 3:
+            return 'Sorry, I don\'t kick people from voice channels ' + \
+                'with less than 3 members.'
+        required_votes = int(voice_members / 2 + 1)
+
+        self.add_vote(target, message.author, channel)
+        vote_count = self.vote_count(target, channel)
+
+        if vote_count >= required_votes:
+            try:
+                await target.move_to(None, 
+                    reason='Democracy is a beautiful thing.')
+            except discord.Forbidden:
+                return 'Tragically, I don\'t have permission to do that.'
+            
+            self.scrub_votes(target)
+            return 'The council has spoken. ' + \
+                f'{target.mention} has been disconnected.'
+        else:
+            needed_votes = required_votes - vote_count
+            return f'Vote received! {needed_votes} more votes required.'            
     
     def add_vote(self, target, sender, channel):
         vote = Vote(target, sender, channel)
@@ -78,8 +75,12 @@ class Vote():
         self.channel = channel if type(channel) == int else channel.id
         self.created = time.time()
 
+    def __repr__(self):
+        return f'<Vote to kick {self.target} at {self.created}' + \
+            f' from {self.sender} in {self.channel}>'
+
     def redundant(self, other):
-        return not (
+        return (
             self.target == other.target and \
             self.sender == other.sender and \
             self.channel == other.channel
