3b4d8ba407822b4fb73181162f9d8d25103492a8
OwenFeik <owenhfeik@gmail.com>
2020-08-01 19:23:04 +1000

    move resources to other directory, update start script

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-cp src/resources/config.json src/resources/config.json.backup
+cp resources/config.json resources/config.json.backup
 git stash
 git pull
-cp src/resources/config.json.backup src/resources/config.json
+cp resources/config.json.backup resources/config.json
 if sudo docker ps | grep -q "owen-bot"; then
 	sudo docker kill "owen-bot"
 fi
 sudo docker build -t "owen-bot" .
-sudo docker run -d -name "owen-bot" -p 80:80 "owen-bot"
+sudo docker run -d -name "owen-bot" -p 80:80 \
+	-v resources:/owen-bot/resources "owen-bot"
