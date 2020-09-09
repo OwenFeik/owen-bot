39fc4bd80e94e57a5e96ff6e8bca7e6d86384717
OwenFeik <owenhfeik@gmail.com>
2020-09-09 13:01:10 +1000

    add blackletter command

--- src/wordart.py
+++ src/wordart.py
@@ -1,6 +1,7 @@
 import json
 import re
 
+import discord
 import emoji
 
 def translate(string, replacement):
@@ -42,12 +43,43 @@ def handle_wordart_request(string, default_emoji):
 
     return translate(string, character)
 
+def scrub_mentions(argument, mentions):
+    other_mentions = re.findall(r'<(@&|#)\d{16,}>', argument)
+    if other_mentions:
+        raise ValueError('Sorry, I don\'t really like mentions.')
+
+    discord_emoji = re.findall(r'<:[\w\W]+:\d{16,}>', argument)
+    if discord_emoji:
+        raise ValueError('Sorry, Discord emotes can\'t be vaporwaved.')
+
+    user_mentions = re.findall(r'<@!?\d{16,}>', argument)
+    for m in user_mentions:
+        user_id = int(m[3:-1]) if m.startswith('<@!') else int(m[2:-1])
+        user = discord.utils.find(
+            lambda m, i=user_id: m.id == i,
+            mentions
+        )
+
+        if user:
+            argument = argument.replace(m, user.display_name)
+        else:
+            raise ValueError('Sorry, I don\'t really like mentions.')
+    
+    return argument
+
 def vaporwave(string):
     normal = u' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
     wide = u'　０１２３４５６７８９ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！゛＃＄％＆（）＊＋、ー。／：；〈＝〉？＠［\\］＾＿‘｛｜｝～'
     widemap = dict((ord(x[0]), x[1]) for x in zip(normal, wide))
 
     return string.translate(widemap)
+
+def blackletter(string):
+    normal = u' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
+    gothic = u' 0123456789𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
+    gothicmap = {ord(n): g for n, g in zip(normal, gothic)}
+
+    return string.translate(gothicmap)
 
 # String sent as message.
 no = '<:urarakagun:637516402885918735>                     <:urarakagun:637516402885918735>    <:urarakagun:637516402885918735><:urarakagun:637516402885918735><:urarakagun:637516402885918735>\n<:urarakagun:637516402885918735><:urarakagun:637516402885918735>        <:urarakagun:637516402885918735>    <:urarakagun:637516402885918735>             <:urarakagun:637516402885918735>\n<:urarakagun:637516402885918735>        <:urarakagun:637516402885918735><:urarakagun:637516402885918735>    <:urarakagun:637516402885918735>             <:urarakagun:637516402885918735>\n<:urarakagun:637516402885918735>                     <:urarakagun:637516402885918735>    <:urarakagun:637516402885918735>             <:urarakagun:637516402885918735>\n<:urarakagun:637516402885918735>                     <:urarakagun:637516402885918735>    <:urarakagun:637516402885918735><:urarakagun:637516402885918735><:urarakagun:637516402885918735>'
